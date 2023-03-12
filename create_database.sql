3116dc48b619d31265bd53bf9d528c42b538fbe4
malrau <maurizio.larosa@unime.it>
2023-03-12 15:06:48 +0100

    Add code to create database schema (database and tables)

---
+++ create_database.sql
@@ -0,0 +1,84 @@
+CREATE DATABASE cb_collection;
+
+CREATE TABLE character(
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    pseudonym VARCHAR(20),
+    PRIMARY KEY(pseudonym)
+    );
+    
+CREATE TABLE comic_book(
+    series VARCHAR(30),
+    issueNumber INT,
+    coverTitle VARCHAR(50),
+    PRIMARY KEY(series, issueNumber)
+    );
+
+CREATE TABLE starring(
+    comic_book VARCHAR(50),
+    issue INT,
+    character VARCHAR(20),
+    role VARCHAR(12),
+    city VARCHAR(30),
+    state VARCHAR(30),
+    PRIMARY KEY(comic_book, issue, character),
+    FOREIGN KEY(comic_book, issue) 
+        REFERENCES comic_book(series, issueNumber)
+        ON DELETE no action,
+        ON UPDATE cascade,
+    FOREIGN KEY(character)
+        REFERENCES character(pseudonym)
+        ON DELETE no action,
+        ON UPDATE cascade
+    );
+
+CREATE TABLE editor(
+    name VARCHAR(30),
+    city VARCHAR(30),
+    PRIMARY KEY(name)
+    );
+
+CREATE TABLE publishing(
+    editor VARCHAR(30),
+    comic_book VARCHAR(30),
+    issue INT,
+    PRIMARY KEY(editor, comic_book, issue),
+    FOREIGN KEY(editor)
+        REFERENCES editor(name)
+        ON DELETE no action
+        ON UPDATE cascade,
+    FOREIGN KEY(comic_book, issue)
+        REFERENCES comic_book(series, issueNumber)
+        ON DELETE no action
+        ON UPDATE cascade
+    );
+
+CREATE TABLE writer(
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    pseudonym VARCHAR(30),
+    PRIMARY KEY(firstName, lastName)
+    );
+
+CREATE TABLE artist(
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    pseudonym VARCHAR(30),
+    PRIMARY KEY(firstName, lastName)
+    );
+
+CREATE TABLE authoring(
+    writerFirstName VARCHAR(30),
+    writerLastName VARCHAR(30),
+    artistFirstName VARCHAR(30),
+    artistLastName VARCHAR(30),
+    PRIMARY KEY(writerFirstName, writerLastName, artistFirstName, artistLastName),
+    FOREIGN KEY(writerFirstName, writerLastName)
+        REFERENCES writer(firstName, lastName)
+        ON DELETE no action
+        ON UPDATE cascade,
+    FOREIGN KEY(artistFirstName, artistLastName)
+        REFERENCES artist(firstName, lastName)
+        ON DELETE no action
+        ON UPDATE cascade
+    );
