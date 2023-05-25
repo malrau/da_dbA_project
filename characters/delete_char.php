b0fecf9366f83ead140beb4877668cc40eab258a
Maurizio La Rosa <mau@maurizios-mbp.homenet.telecomitalia.it>
2023-05-25 19:54:17 +0200

    Create code for character deletion

---
+++ characters/delete_char.php
@@ -0,0 +1,77 @@
+<html>
+	<header>
+		<title>
+			Webapp - character deletion page
+		</title>
+	</header>
+	
+	<body>
+		<p>
+			<?php
+				echo "<p>";
+				echo "<center>";
+				echo "<h2>In this page you can delete comic book characters <br> in your database</h2>";
+				echo "</center>";
+				echo "</p>";
+
+				mysqli_report(MYSQLI_REPORT_ERROR);
+				
+				// return settings from configuration file into an associative array
+				$config = parse_ini_file('../config.ini');
+				// retrieve parameters to access MySQL from associative array and
+				// assign them to PHP variables
+				$servername = 'servername';
+				$username = 'username';
+				$password = 'password';
+				$dbname = 'dbname';
+				
+				// create and check connection to MySQL
+				$conn = mysqli_connect($servername, $username, $password, $dbname);
+				if(!$conn) {
+					echo "<h3>Cannot connect to MySQL: </h3>" . mysqli_connect_error();
+					exit;
+				} // else {
+//					echo "<h3>Successfully connected to MySQL.</h3>";
+//				}
+// 				the above three lines are commented because I don't want 
+//				this message to be shown in the page where the query is set
+				
+				// define query to retrieve all results from the table 
+				// figure and store the query result
+				$sql = 'SELECT * FROM figure';
+				$result = mysqli_query($conn, $sql);
+				
+				// check if figure table is empty
+				if(result) {
+					if(mysqli_fetch_row($result) > 0) {
+						echo "<p>";
+						echo "<h4>Delete character</h4>";
+						echo "<form method = 'post' action = 'char_deletion.php' id = 'submit character'";
+						echo "<select name = 'character'>";
+						while($row = mysqli_fetch_array($result)) {
+							$character = $row[0] . ' ' . $row[1];
+							echo "<option value = '$character'>";
+							echo $character;
+							echo "</option>";
+						}
+						echo "</select>";
+						echo "<input type = 'submit' value = 'submit character'>";
+						echo "</form>";
+						echo "</p>";
+						mysqli_free_result($result);
+					} else {
+						echo "<h3>No matching records are found</h3>";
+				} else {
+					echo "<h3>ERROR. Cannot execute $sql.</h3>" . mysqli_error($conn);
+				}
+			?>
+		</p>
+		
+		<br>
+		
+		<p>
+			<a href = '_a_characters.html'><button>Back to the characters page</button></a>
+			<a href = '../index.html'><button>Back to the main page</button></a>
+		</p>
+	</body>
+</html>
