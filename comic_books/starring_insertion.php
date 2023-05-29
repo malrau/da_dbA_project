9292022ac794faac56cf74115ad74b3fdba80763
malrau <maurizio.larosa@unime.it>
2023-05-29 16:16:42 +0200

    Disable script in starring_insertion.php, connect to MySQL the old way

--- comic_books/starring_insertion.php
+++ comic_books/starring_insertion.php
@@ -7,9 +7,30 @@
 	
 	<body>
 		<?php
-			// exploit script to perform MySQL connection
-			include('../connect.php');
+			# exploit script to perform MySQL connection (commented, at the moment)
+			# include('../connect.php');
 			
+			// the following line allows me to report connection errors,
+			// otherwise a blank page would occur in case of errors
+			mysqli_report(MYSQLI_REPORT_ERROR);
+
+			// return settings from configuration file into an associative array
+			$config = parse_ini_file('../config.ini');
+			// get parameters to access MySQL from associative array and
+			// assign them to PHP variables
+			$servername = $config['servername'];
+			$username = $config['username'];
+			$password = $config['password'];
+			$dbname = $config['dbname'];
+
+			// create and check connection to MySQL
+			$conn = mysqli_connect($servername, $username, $password, $dbname);
+			if(!$conn) {
+				echo "<h3>Cannot connect to MySQL: </h3>" . mysqli_connect_error();
+				exit;
+			} else {
+				echo "<h3>Successfully connected to MySQL.</h3>";
+			}
 			
 			// assign data to PHP variables
 			$cbID = $_POST['cbID'];
