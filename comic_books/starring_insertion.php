c2047005386e5c74c65257245304fcd8b5802336
malrau <maurizio.larosa@unime.it>
2023-05-29 16:11:47 +0200

    Test MySQL connection from script and perform starring table insertion

---
+++ comic_books/starring_insertion.php
@@ -0,0 +1,50 @@
+<html>
+	<head>
+		<title>
+			Inserted starring data
+		</title>
+	</head>
+	
+	<body>
+		<?php
+			// exploit script to perform MySQL connection
+			include('../connect.php');
+			
+			
+			// assign data to PHP variables
+			$cbID = $_POST['cbID'];
+			#echo $cbID;
+			$city = $_POST['city'];
+			$country = $_POST['country'];
+			#echo $city;
+			#echo $country;
+
+			foreach($_POST as $key => $value) {
+				if($dashPosition = strpos($key, '-')) {
+					$pseudoKey = substr($key, 0, $dashPosition);
+					/*  note that $pseudoKey, within the $_POST array is
+						key and value at the same time */
+					$pseudonym = $_POST[$pseudoKey];
+					#echo "PSEUDO " . $pseudonym;
+					$tempRole = substr($key, $dashPosition + 1);
+					/*	I reconstruct the role key to retrieve the corresponding
+						value from the $_POST array */
+					$figureRole = $_POST[$pseudoKey . '-' . $tempRole];
+					#echo "ROLE " . $figureRole;
+					# perform insertion into 'starring' for each character
+					$sql = "INSERT INTO starring(comic_book, figure, figureRole, city, country) VALUES('$cbID', '$pseudonym', '$figureRole', '$city', '$country')";
+					if(mysqli_query($conn, $sql)) {
+						echo "<h3>Data was successfully inserted into the <i>starring</i> table.</h3>";
+					} else {
+						echo "<h3>ERROR! Could not insert data into the <i>starring</i> table: </h3>" . mysqli_error($conn);
+					}
+				} else {
+					echo "<h3>No character and role data found.</h3>";
+				}
+			}
+			
+
+
+		?>
+	</body>
+</html>
