ec56145730ddd4fd261baf53b7b0a3e2e09ab23e
malrau <maurizio.larosa@unime.it>
2023-06-01 10:56:26 +0200

    Add <br> at the end of the script, so to better separate connection result from the rest of the page

--- connect.php
+++ connect.php
@@ -1,31 +1,33 @@
 <?php
-	# Script to perform connection to a MySQL server by retrieving data from
-	# '.ini' configuration file
+	/*
+	*** Script to perform connection to a MySQL server by retrieving data ***
+	*** from '.ini' configuration file				      ***
+	*/
 
-	// the following line allows me to report connection errors, otherwise
-	// a blank page would occur in case of errors
+	/* the following line allows me to report connection errors, otherwise
+	   a blank page would occur in case of errors */
 	mysqli_report(MYSQLI_REPORT_ERROR);
 
-	# ********************
-	# ****** STEP 1 ******
-	# ********************
-	// return settings from configuration file into an associative array
+	/********************
+	 ****** STEP 1 ******
+	 ********************/
+	# return settings from configuration file into an associative array
 	$config = parse_ini_file('config.ini');
 
-	# ********************
-	# ****** STEP 2 ******
-	# ********************
-	// get parameters to access MySQL from associative array and assign them
-	// to PHP variables
+	/********************
+	 ****** STEP 2 ******
+	 ********************/
+	/* get parameters to access MySQL from associative array and assign them
+	   to PHP variables */
 	$servername = $config['servername'];
 	$username 	= $config['username'];
 	$password 	= $config['password'];
 	$dbname 	= $config['dbname'];
 
-	# ********************
-	# ****** STEP 3 ******
-	# ********************
-	// create and check connection to MySQL by using the above defined PHP variables
+	/********************
+	 ****** STEP 3 ******
+	 ********************/
+	# create and check connection to MySQL by using the above defined PHP variables
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	if(!$conn) {
 		echo "<h4>Cannot connect to MySQL: </h4>" . mysqli_connect_error();
@@ -33,4 +35,6 @@
 	} else {
 		echo "<h4>Successfully connected to MySQL.</h4>";
 	}
+
+	echo "<br>";
 ?>
