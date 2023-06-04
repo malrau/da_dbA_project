d32a7938efc43ce623b7f94d3bbd9c277cc2bfe2
malrau <maurizio.larosa@unime.it>
2023-06-04 11:08:02 +0200

    Change comic books update code and file references

---
+++ comic_books/_d_update_cb.php
@@ -0,0 +1,82 @@
+<html>
+	<head>
+		<title>
+			Webapp - comic book update page
+		</title>
+	</head>
+
+	<body>
+		<?php
+			# exploit script to perform MySQL connection
+			include('../connect.php');
+
+			echo "<p>";
+			echo "<center>";
+			echo "<h2>In this page you can update data <br> in your comic books collection</h2>";
+			echo "</center>";
+			echo "</p>";
+
+			# query the comic_book table for all results
+			$sql = "SELECT * FROM comic_book";
+
+			# check that the above defined query can be performed
+			if($result = mysqli_query($conn, $sql)) {
+				if(mysqli_num_rows($result) > 0) {
+					/* if the query result is not empty (there is data
+					   in the comic_book table), create a form to: */
+
+					# 1) check the comic book data that need be updated
+					echo "<p>";
+					echo "<h4>Check the comic book data that you want to update:</h4>";
+					echo "<form method = 'post' action = 'cb_update.php'>";
+					echo "<p>";
+					echo "<select name = 'comic_book'>";
+					while($row = mysqli_fetch_array($result)) {
+						$comic_book = $row[1] . ' No. ' . $row[2] . ' - ' . $row[3];
+						echo "<option value = '$comic_book'>";
+						echo $comic_book;
+						echo "</option>";
+					}
+					echo "</select>";
+					echo "</p>";
+					echo "<br>";
+
+					# 2) choose the attribute you want to update
+					echo "<p>";
+					echo "Choose the comic book attribute you want to update: ";
+					echo "<input type = 'radio' name = 'attribute' value = 'series'>series; ";
+					echo "<input type = 'radio' name = 'attribute' value = 'issueNumber'>issue number; ";
+					echo "<input type = 'radio' name = 'attribute' value = 'coverTitle'>cover title";
+					echo "</p>";
+
+					# 3) indicate old and new value for the chosen attribute
+					echo "<p>";
+					echo "Insert the old value for the selected attribute: ";
+					echo "<input type = 'text' name = 'old'>";
+					echo "<br>";
+					echo "Insert the new value for the selected attribute: ";
+					echo "<input type = 'text' name = 'new'>";
+					echo "</p>";
+
+					echo "<p>";
+					echo "<input type = 'submit' value = 'update data'>";
+					echo "<input type = 'reset' value = 'reset fields'>";
+					echo "</p>";
+
+					echo "</form>";
+				} else {
+					echo "<h4>No matching records are found</h4>";
+				}
+			} else {
+				echo "<h3>ERROR. Cannot execute $sql: </h3>" . mysqli_error($conn);
+			}
+		?>
+
+		<br>
+
+		<p>
+			<a href = '_a_comic_books.html'><button>Back to the comic books page</button></a>
+			<a href = '../index.html'><button>Back to the main page</button></a>
+		</p>
+	</body>
+</html>
--- comic_books/cb_update.php
+++ comic_books/cb_update.php
@@ -11,6 +11,7 @@
 			include('../connect.php');
 
 			# assign submitted data to PHP variables
+			$comic_book = $_POST['comic_book']; // VARIABLE NOT USED
 			$attribute = $_POST['attribute'];
 			$old = $_POST['old'];
 			$new = $_POST['new'];
@@ -18,16 +19,16 @@
 			# perform and check update of the comic book table
 			$sql = "UPDATE comic_book SET $attribute = '$new' WHERE $attribute = '$old'";
 			if(mysqli_query($conn, $sql)) {
-				echo "<h3>Data from table <i>comic_book</i> was successfully updated</h3>";
+				echo "<h3>Attribute $attribute from table <i>comic_book</i> has been successfully updated</h3>";
 			} else {
-				echo "<h3>ERROR! Could not update data from table <i>comic_book</i></h3>" . mysqli_error($conn);
+				echo "<h3>ERROR! Could not update attribute $attribute from table <i>comic_book</i></h3>" . mysqli_error($conn);
 			}
 		?>
 
 		<br>
 		
 		<p>
-			<a href = '_d_update_cb.html'><button>Back to the update comic book page</button></a>
+			<a href = '_d_update_cb.php'><button>Back to the update comic book page</button></a>
 			<a href = '_a_comic_books.html'><button>Back to the comic books page</button></a>
 			<a href = '../index.html'><button>Back to the main page</button></a>
 		</p>
